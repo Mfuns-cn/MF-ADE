2328a720792103a02ca56fc1158fb07f9195e535
chenda <1163485800@qq.com>
2021-08-01 13:16:30 +0800

    删除本地插件代码

--- src/player/mfunsPlayer/js/danmakuEditor.js
+++ src/player/mfunsPlayer/js/danmakuEditor.js
@@ -1,55 +1,66 @@
-import Template from "./getDOM.js"
-let tem
-export function openEditor(){
-	tem = Template()
-	tem.danmakuList.style.animation = 'retract 0.3s'
-	tem.danmakuList.style.display = 'none'
-	tem.video.pause()
-	tem.ade_mask.style.display = 'block'
-	tem.ade.style.animation = 'down 0.35s'
+import Template from "./getDOM.js";
+let tem;
+export function addAcejs() {
+  return new Promise((resolve, reject) => {
+    var acejs = document.createElement("script");
+    acejs.setAttribute("src", "http://cdn.bootcss.com/ace/1.4.9/ace.js");
+    document.body.appendChild(acejs);
+    var lan_tool = document.createElement("script");
+    lan_tool.setAttribute(
+      "src",
+      "http://cdn.bootcss.com/ace/1.4.9/ext-language_tools.js"
+    );
+    document.body.appendChild(lan_tool);
+    // function checkAce() {
+    //   setTimeout(() => {
+    //     if (window.ace) {
+    //       resolve();
+    //     } else {
+    //       checkHLs();
+    //       // reject()
+    //     }
+    //   }, 10);
+    // }
+    // checkAce();
+  });
+}
+export function openEditor() {
+  tem = Template();
+  tem.danmakuList.style.animation = "retract 0.3s";
+  tem.danmakuList.style.display = "none";
+  tem.video.pause();
+  tem.ade_mask.style.display = "block";
+  tem.ade.style.animation = "down 0.35s";
 }
 
-export function closeEditor(){
-	tem.video.play()
-	tem.ade.style.animation = 'up 0.35s'
-	setTimeout(() => {
-		tem.ade_mask.style.display = 'none'
-	}, 350)
+export function closeEditor() {
+  tem.video.play();
+  tem.ade.style.animation = "up 0.35s";
+  setTimeout(() => {
+    tem.ade_mask.style.display = "none";
+  }, 350);
 }
-export function createPreview(){
-	return new Promise((resolve, reject) => {
-	
-	tem.advancePre.style.width = tem.content.clientWidth + 'px'
-	tem.advancePre.style.height = tem.video.clientHeight + 'px'
-
-	
-	if(tem.advancePre){
-		resolve(tem)
-	}else{
-		reject()
-	}
-	})
-	
+export function createPreview() {
+  return new Promise((resolve, reject) => {
+    tem.advancePre.style.width = tem.content.clientWidth + "px";
+    tem.advancePre.style.height = tem.video.clientHeight + "px";
+    if (tem.advancePre) {
+      resolve(tem);
+    } else {
+      reject();
+    }
+  });
 }
-
 
 export function previewCallback(code) {
-	let advanceDanmakuPreview 
-	if(!advanceDanmakuPreview ){
-		advanceDanmakuPreview = new MfunsDanMaku({
-		containers: tem.advancePre,
-		danmaku: send => {
-			send([code])
-		}
-	})
-	}
-	return advanceDanmakuPreview 
-}
-
-export function clearCallback() {
-
-}
-
-export function emitCallback() {
-
+  let advanceDanmakuPreview;
+  if (!advanceDanmakuPreview) {
+    advanceDanmakuPreview = new MfunsDanMaku({
+      containers: tem.advancePre,
+      danmaku: (send) => {
+        send([code]);
+      },
+    });
+  }
+  return advanceDanmakuPreview;
 }
--- src/player/mfunsPlayer/js/index.js
+++ src/player/mfunsPlayer/js/index.js
@@ -1,28 +1,26 @@
-import {init} from "./control.js"
-import {addHls} from "./MSE.js"
-// import '../css/index.css'
-import './ace/ace.js'
-import './util/flv.js'
- export default class mfunsPlayer{
-	constructor(el,url,callback) {
-           
-		   el.style.background = "#000"
-		     el.style.minWidth = "400px"
-	       el.style.position = 'relative'
-		   el.style.display = 'flex'
-		   el.style.justifyContent = 'center'
-		   el.style.overflow = 'hidden'
-		   addHls().then(res=>{
-			   console.log('hls引入成功')
-			   init(el,url,callback) 
-		   })
-	}
+import { init } from "./control.js";
+import { addAcejs } from "./danmakuEditor.js";
+import "../css/index.css";
+// import './ace/ace.js'
+// import './util/flv.js'
+class mfunsPlayer {
+  constructor(el, url, callback) {
+    addAcejs();
+    el.style.background = "#000";
+    el.style.minWidth = "400px";
+    el.style.position = "relative";
+    el.style.display = "flex";
+    el.style.justifyContent = "center";
+    el.style.overflow = "hidden";
+    init(el, url, callback);
+  }
 }
 
 if (globalThis) {
-	globalThis.mfunsPlayer = mfunsPlayer
-	console.log(`${'\n'} %c mfunsPlayer v1.0.0 %c https://github.com/Mfuns-cn ${'\n'}${'\n'}`, 
-	'color: #fff; background: #7b7ff7; padding:5px 0;', 'background: #f5f5f5; padding:5px 0;');
- }
-
- 
+  globalThis.mfunsPlayer = mfunsPlayer;
+  console.log(
+    `${"\n"} %c mfunsPlayer v1.0.0 %c https://github.com/Mfuns-cn ${"\n"}${"\n"}`,
+    "color: #fff; background: #7b7ff7; padding:5px 0;",
+    "background: #f5f5f5; padding:5px 0;"
+  );
+}
